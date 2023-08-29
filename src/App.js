a903a7945ef1ce6e1d720176e65cc8e66d4993c3
Nikunj Singhania <nikunj05108@gmail.com>
2023-08-29 16:36:55 +0530

    HOOKS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const initialCount = 0
+  const [arr, setArr] = useState([1, 2, 3])
+  const [data, setData] = useState(initialCount)
+  const [obj, setObj] = useState({
+    name : 'Nikunj Singhania',
+    age : 22
+  })
+  const incFive = () => {
+
+    for(let i=1;i<=5;i++) {
+      //setData(data+1)
+      setData(prevData => prevData + 1);
+    }
+  }
+
+  const add = () => {
+    setArr([...arr, Math.random()]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setData(data+1)}>Inc : {data}</button>
+      <button onClick={() => setData(data-1)}>Dec : {data}</button>
+      <button onClick={() => setData(initialCount)}>Reset : {data}</button>
+      <button onClick={incFive}>INC 5</button>
+
+
+      <input type='text' onChange={(e) => setObj({ ...obj,  name : e.target.value })}/>
+      <p>{obj.name} {obj.age}</p>
+      {
+        arr.map((e) => {
+          return <h1>{e}</h1>
+        })
+      }
+
+      <button onClick={add}>Add</button>
     </div>
   );
 }
